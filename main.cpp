59f607d856ce897fc6f30bbafdcbfac4f28893af
voidvvv <1@gmail.com>
2024-04-11 10:24:25 +0800

    add openal as music source

--- main.cpp
+++ main.cpp
@@ -4,8 +4,22 @@
 #include <locale>
 #include "utf8.h"
 
-int main (){
+#include <AL/alut.h>
 
+void sayHu(){
+      ALuint helloBuffer, helloSource;
+  alutInit (NULL, NULL);
+  helloBuffer = alutCreateBufferHelloWorld ();
+  alGenSources (1, &helloSource);
+  alSourcei (helloSource, AL_BUFFER, helloBuffer);
+  alSourcePlay (helloSource);
+  alutSleep (1);
+  alutExit ();
+  return ;
+};
+
+int main (){
+    sayHu();
 // setlocale(LC_ALL, "chs");
 // setlocale(LC_ALL, "C");
     std::cout << "游戏开始" << std::endl;
